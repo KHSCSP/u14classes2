ad19cf594ad3269bc16f2a9497b0c107b7f4bd2e
heyscottn <heyscottn@gmail.com>
2024-02-28 12:13:35 -0600

    mm

---
+++ apply.py
@@ -0,0 +1,23 @@
+from student_class import Student
+
+# the student data is
+# name, HW1, HW2, HW3, Test
+
+# get data, split at commas!, add to list
+
+
+
+
+options = '''
+\nHere are your options:
+(show) to show all student data
+(df) to show a report of all students with a d or f on test
+(honor) to show all students with a test >= 85
+(test) to show the average of all tests
+(hw) to show the average of all HW assignments
+(one) to show one specific student's info
+(q) to quit
+'''
+
+# TODO program logic
+
